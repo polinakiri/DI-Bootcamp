5936b9fdc56c506c08edb1eb48ec178615be7df3
Polina <polina.a.kiri@gmail.com>
2023-01-23 09:22:55 +0200

    added daily challenge

---
+++ Week2/Day1/Daily/script.js
@@ -0,0 +1,23 @@
+//Exercise 1:
+
+// Remove Banana from the array.
+// Sort the array in alphabetical order.
+// Add “Kiwi” to the end of the array.
+// Remove “Apples” from the array. Don’t use the same method as in part 1.
+// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+fruits.sort();
+fruits.push("Kiwi");
+fruits.splice(0,1);
+fruits.reverse();
+
+//Exercise 2:
+
+//Access and then console.log “Oranges”.
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]);
