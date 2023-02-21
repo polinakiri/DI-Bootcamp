c762ed1824117eeab2c5a42946f9eb62f8a89581
Polina <polina.a.kiri@gmail.com>
2023-02-21 11:29:33 +0200

    added exercises XP

---
+++ Week5/Day1/XP/ex3.js
@@ -0,0 +1,32 @@
+// Exercise 3 : JSON Mario
+
+// Convert this JS object into a JSON object. What happens to the nested objects ?
+// Convert and pretty print this JS object into a JSON object. Hint : Check out the JSON lesson on the platform.
+// Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
+
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+  const obj = JSON.stringify(marioGame);
+  console.log(obj);
